3a31d752bf61d67b361d60dc150a1d2c0ee08a9d
Nadia HYLARY <nhylary12@gmail.com>
2023-03-13 08:43:39 +0100

    added form validation

--- reviews/views.py
+++ reviews/views.py
@@ -1,15 +1,31 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from .forms import ReviewForm
 # Create your views here.
 
 
 def review(request):
-    if request.method == "POST":
-        entered_username = request.POST["username"]
-        return HttpResponseRedirect("/thanks")
+    # if request.method == "POST":
+    #     entered_username = request.POST["username"]
+    #     if entered_username == "":
+    #         return render(request, "reviews/review.html", {
+    #             "has_error": True
+    #         })
+    #     return HttpResponseRedirect("/thanks")
+    # return render(request, "reviews/review.html", {
+    #     "has_error": True
+    # })
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("/thanks")
+
     else:
-        return render(request, "reviews/review.html")
+        form = ReviewForm()
+
+    return render(request, "reviews/review.html", {
+        "form": form
+    })
 
 
 def thankyou(request):
